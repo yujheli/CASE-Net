b9ff11956d6fdcaa1cd6cf0a013b50d20c716f8d
Yun-Chun Chen <b03901148@ntu.edu.tw>
2018-08-06 14:03:36 +0800

    gen data

--- data/csv/CUHK03/gen_data.py
+++ data/csv/CUHK03/gen_data.py
@@ -17,19 +17,18 @@ with open('train_list_2_4_8.csv', 'w') as f:
     for line in data:
         f.write(line)
 
-with open('query_list_4.csv', 'r') as f:
+
+data = []
+
+with open('query_list.csv', 'r') as f:
     title = f.readline()
     for line in f.readlines():
         token = line[:-1]
         img, ID, cam, _ = token.split(',')
-        gg = img + ',' + ID + ',' + cam + ',2\n'
-        data.append(gg)
-        gg = img + ',' + ID + ',' + cam + ',4\n'
-        data.append(gg)
         gg = img + ',' + ID + ',' + cam + ',8\n'
         data.append(gg)
 
-with open('query_list_2_4_8.csv', 'w') as f:
+with open('query_list_8.csv', 'w') as f:
     f.write(title)
     for line in data:
         f.write(line)
